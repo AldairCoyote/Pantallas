b4e295a3a949725761dfad2529988adc1d9662a0
Choca <acoyote@xiot.com.mx>
2019-02-18 17:29:15 -0600

    se mejoro el js

--- js/prueba.js
+++ js/prueba.js
@@ -1,24 +1,55 @@
-//   inicia materialize
-  document.addEventListener('DOMContentLoaded', function() {
-    M.AutoInit()
+//inicia materialize
+$(document).ready(function(){
+$('select').formSelect()
+  genPedidos()
+  M.AutoInit()
+})
+//genera los pedidos
+function genPedidos(){
+  $.get('prueba.json',
+  function(data){
+    var tbl = data.Rowsets.Rowset[0]
+    var lgn = tbl.Row.length
+    //muestra los primeros 3 pedidos
+    for(i=0; i<3; i++){
+      $("#psd").append('<tr>' + 
+      '<td>' +  + tbl.Row[i].MATERIAL + '</td>'+
+      '<td>' + tbl.Row[i].DESCRIPCION + '</td>'+
+      '<td>' + tbl.Row[i].LOTE + '<span class="new badge red right" data-badge-caption="No Asignado">' + '</span>' + '</td>' + '</tr>')
+    }
+
+    //genera la lista de pedidos
+    for(i=0; i<lgn; i++){
+      $("#pedidos").append(
+      '<a class="bttn waves-effect collection-item waves-light black-text">' +
+      '<li>' + tbl.Row[i].MATERIAL + '</li>' +
+      '<li>' + tbl.Row[i].DESCRIPCION + '</li>' + 
+      '<li>' + tbl.Row[i].ORDEN + '</li>' +
+      '<li>' + tbl.Row[i].LOTE + '</li>' +
+      '<li>' + tbl.Row[i].CADUCIDAD + '</li>' 
+      + '</a>')
+    }
+
+    //genera los detalles de los pedidos
+      $('.bttn').click(function(data){
+      $('#tabla').empty('tr')
+      var tot
+      tot = $(this).text()
+      $("#tabla").append('<tr>' +
+       '<td>' + tot + '</td>' + '</tr>')
+     })
+
+     //semaforo
+     $('#prueba').click(function(){
+      $('#prueba').removeClass('red')
+      $('#prueba').addClass('green')
+     })
   })
+}
 
-  $(document).ready(function(){
-    $('select').formSelect()
-    genPedidos()
-    PDV()
-  })
-
-  // semaforo
-  function cambiarColor() {
-        document.getElementById('prueba')
-        prueba.classList.remove('red')
-        prueba.classList.add('green')
-      }
-
-  // despliega la lista de puertas
-  function asignarPuerta(){
-  var p = $.get('puertas.json',
+ //despliega la lista de puertas
+ function asignarPuerta(){
+  $.get('puertas.json',
   function(data){
     var tbl = data.Rowsets.Rowset[0]
     var lgn = tbl.Row.length
@@ -30,90 +61,47 @@
 }
 asignarPuerta()
 
-// $(document).ready(function(){
-//   $.ajax({url: "puertas.json", success: function(result){
-//     $("#puertas").append('<option '+'value='+[i]+'>' +
-//      'PUERTA '+ tbl.Row[i].PUERTA + '</option>').html(result);
-//   }});
-// });
 
-// despliega la lista de pedidos
-function genPedidos(){
-  $.get('prueba.json',
-  function(data){
-    var tbl = data.Rowsets.Rowset[0]
-    var lgn = tbl.Row.length
-    for(i=0; i<lgn; i++){
-      $("#pedidos").append('<a class="bttn waves-effect waves-light red">' + 
-      + tbl.Row[i].ORDEN + '//'+ tbl.Row[i].LOTE + '</a>')
-    }
-    $('.bttn').click(function(){
-      var tot
-      var c1 = tbl.Row[i]
-      tot=$(this).text()
-       console.log(tot)
-       console.log(c1)
-     })
-  })
-}
-
-// Obtenga los antepasados ​​de cada elemento en el conjunto actual de elementos emparejados, opcionalmente filtrado por un selector.
-// Obtenga los descendientes de cada elemento en el conjunto actual de elementos emparejados, filtrados por un selector, un objeto jQuery o un elemento.
-// Iterar sobre un objeto jQuery, ejecutando una función para cada elemento coincidente.
-
-// muestra los detalles de pedidos
-function mostrarPedido(){
-  var mtr = document.getElementById('tabla')
-
-  if(mtr.hasChildNodes()){
-    mtr.removeChild(mtr.firstChild)
-  }
-  var x = $.get('prueba.json',
-  function(data){
-    var tbl = data.Rowsets.Rowset[0]
-    var lrg = tbl.Row.length
-      for(i = 0; i < lrg; i++){
-        $("#tabla").append('<tr id="ped' + [i] + '">' +
-        '<td>' + tbl.Row[i].MATERIAL + '</td>'+
-        '<td>' + tbl.Row[i].DESCRIPCION + '</td>'+
-        '<td>' + tbl.Row[i].ORDEN + '</td>'+
-        '<td>' + tbl.Row[i].LOTE + '</td>'+
-        '<td>' + tbl.Row[i].CADUCIDAD + '</td>' + '</tr>')
-      }
-    })
-  }
-  // Obtenga los antepasados ​​de cada elemento en el conjunto actual de elementos emparejados, opcionalmente filtrado por un selector.
-  // Obtenga los descendientes de cada elemento en el conjunto actual de elementos emparejados, filtrados por un selector, un objeto jQuery o un elemento.
-  // Iterar sobre un objeto jQuery, ejecutando una función para cada elemento coincidente.
-
-
-// muestra los primeros 3 pedidos no asignados
-function PDV(){
-  $.get('prueba.json',
-  function(data){
-    var tbl = data.Rowsets.Rowset[0]
-    var lgn = 3
-    for(i = 0; i < lgn; i++){
-      $("#psd").append('<tr>' + 
-      '<td>' +  + tbl.Row[i].MATERIAL + '</td>'+
-      '<td>' + tbl.Row[i].DESCRIPCION + '</td>'+
-      '<td>' + tbl.Row[i].LOTE + '<span class="new badge red right" data-badge-caption="No Asignado">' + '</span>' + '</td>' + '</tr>')
-    }
-  })
-}
-
-// function genTable(){ new badge red data-badge-caption="no asignado"
-//   $.get('prueba.json',
-//   function(data){
-//     var tbl = data.Rowsets.Rowset[0]
-//     var lgn = tbl.Row.length
-//     for(i=0; i<lgn; i++){
-//       $("#psd").append('<tr>' + 
-//       '<td align="center">' + 'hola' + tbl.Row[i].MATERIAL + '</td>'+
-//       '<td align="center">' + tbl.Row[i].DESCRIPCION + '</td>'+
-//       '<td align="center">' + tbl.Row[i].ORDEN + '</td>'+
-//       '<td align="center">' + tbl.Row[i].LOTE + '</td>'+
-//       '<td align="center">' + tbl.Row[i].CADUCIDAD + '</td>'+'</tr>')
+// const data = {
+//   "json": [
+//     {
+//       "list_ip": [
+//         {
+//           "1": "190.15.20.18",
+//           "2": "10.10.10.100",
+//           "3": "10.20.30.40"
+//         }
+//       ],
+//       "cosas": [
+//         {
+//           "elementos": [
+//              {
+//                "ip_id": 1
+//              },
+//              {
+//                "ip_id": 3
+//              }
+//           ]
+//         }
+//       ]
 //     }
-//   })
+//   ]
+// };
+
+// function mapIPs() {
+//   const ips = data.json[0].list_ip[0];
+//   const els = data.json[0].cosas[0].elementos;
+//   const associations = {};
+  
+//   els.forEach(el => {
+//     for(let [key, val] of Object.entries(el)) {
+//       if (ips[val]) {
+//         associations[val] = ips[val];
+//       }
+//     }
+//   });
+  
+//   return associations;
 // }
+
+// console.info(mapIPs());
