a2a0caf41f439cb46bd555b6e05c2fbf2b68d8f6
Choca <acoyote@xiot.com.mx>
2019-02-19 14:25:26 -0600

    se muestra la tabla de detalles

--- js/prueba.js
+++ js/prueba.js
@@ -6,43 +6,54 @@ $('select').formSelect()
 })
 //genera los pedidos
 function genPedidos(){
-  $.get('prueba.json',
+  var json = $.get('prueba.json',
   function(data){
     var tbl = data.Rowsets.Rowset[0]
     var lgn = tbl.Row.length
     //muestra los primeros 3 pedidos
     for(i=0; i<3; i++){
-      $("#psd").append('<tr>' + 
-      '<td>' +  + tbl.Row[i].MATERIAL + '</td>'+
+      $('#psd').append('<tr>' + 
+      '<td>' + tbl.Row[i].MATERIAL + '</td>'+
       '<td>' + tbl.Row[i].DESCRIPCION + '</td>'+
       '<td>' + tbl.Row[i].LOTE + '<span class="new badge red right" data-badge-caption="No Asignado">' + '</span>' + '</td>' + '</tr>')
     }
 
     //genera la lista de pedidos
     for(i=0; i<lgn; i++){
-      $("#pedidos").append(
-      '<a class="bttn waves-effect collection-item waves-light black-text">' +
-      '<li>' + tbl.Row[i].MATERIAL + '</li>' +
-      '<li>' + tbl.Row[i].DESCRIPCION + '</li>' + 
-      '<li>' + tbl.Row[i].ORDEN + '</li>' +
-      '<li>' + tbl.Row[i].LOTE + '</li>' +
-      '<li>' + tbl.Row[i].CADUCIDAD + '</li>' 
-      + '</a>')
+      const p = {
+        Material: tbl.Row[i].MATERIAL,
+        Descrip: tbl.Row[i].DESCRIPCION,
+        Orden: tbl.Row[i].ORDEN,
+        Lote: tbl.Row[i].LOTE,
+        Cad: tbl.Row[i].CADUCIDAD
+      }
+
+      $('#pedidos').append(
+      '<a id="ped'+ [i] +'" class="bttn waves-effect collection-item waves-light black-text">' +
+      '<li>' + p.Material + '</li>' +
+      '<li>' + p.Lote + '</li>'+ 
+      '</a>')
+
+      //genera los detalles de los pedidos
+      $('.bttn').click(function(){
+        var str = (this).text
+        if(str === p.Material + p.Lote){
+        $('#tabla').empty('tr')
+        $('#tabla').append('<tr>' + 
+        '<td>' + p.Material + '</td>' +
+        '<td>' + p.Descrip + '</td>' +
+        '<td>' + p.Orden + '</td>' +
+        '<td>' + p.Lote + '</td>' +
+        '<td>' + p.Cad + '</td>' +
+        '</tr>')
+        }
+      })
     }
 
-    //genera los detalles de los pedidos
-      $('.bttn').click(function(data){
-      $('#tabla').empty('tr')
-      var tot
-      tot = $(this).text()
-      $("#tabla").append('<tr>' +
-       '<td>' + tot + '</td>' + '</tr>')
-     })
-
      //semaforo
-     $('#prueba').click(function(){
-      $('#prueba').removeClass('red')
-      $('#prueba').addClass('green')
+    $('#prueba').click(function(){
+    $('#prueba').removeClass('red')
+    $('#prueba').addClass('green')
      })
   })
 }
@@ -54,54 +65,9 @@ function genPedidos(){
     var tbl = data.Rowsets.Rowset[0]
     var lgn = tbl.Row.length
     for(i=0; i<lgn; i++){
-      $("#puertas").append(
+      $('#puertas').append(
         '<option '+'value='+[i]+'>' + 'PUERTA '+ tbl.Row[i].PUERTA + '</option>')
     }
   })
 }
 asignarPuerta()
-
-
-// const data = {
-//   "json": [
-//     {
-//       "list_ip": [
-//         {
-//           "1": "190.15.20.18",
-//           "2": "10.10.10.100",
-//           "3": "10.20.30.40"
-//         }
-//       ],
-//       "cosas": [
-//         {
-//           "elementos": [
-//              {
-//                "ip_id": 1
-//              },
-//              {
-//                "ip_id": 3
-//              }
-//           ]
-//         }
-//       ]
-//     }
-//   ]
-// };
-
-// function mapIPs() {
-//   const ips = data.json[0].list_ip[0];
-//   const els = data.json[0].cosas[0].elementos;
-//   const associations = {};
-  
-//   els.forEach(el => {
-//     for(let [key, val] of Object.entries(el)) {
-//       if (ips[val]) {
-//         associations[val] = ips[val];
-//       }
-//     }
-//   });
-  
-//   return associations;
-// }
-
-// console.info(mapIPs());
